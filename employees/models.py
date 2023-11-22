80a43cbb35b9e8861c49f233f889f32e362125db
Arek Sanecki <areksanecki@gmail.com>
2023-11-22 18:49:00 +0100

    dodanie 2 pracowników do bazy

--- employees/models.py
+++ employees/models.py
@@ -11,3 +11,6 @@ class Employee(models.Model):
     phone_number = models.CharField(max_length=12, blank = True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.first_name +" "+ self.last_name
